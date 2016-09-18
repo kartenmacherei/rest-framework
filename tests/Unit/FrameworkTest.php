0f70d9a84bb82f2e3cc7ebad7bfe2b8f45f2d971
Sebastian Heuer <sebastian@phpeople.de>
2016-09-18 17:53:03 +0200

    add unit tests for Framework

---
+++ tests/Unit/FrameworkTest.php
@@ -0,0 +1,180 @@
+<?php
+namespace Kartenmacherei\RestFramework\UnitTests;
+
+use Kartenmacherei\RestFramework\Action\Command\CommandLocator;
+use Kartenmacherei\RestFramework\Action\NoMoreLocatorsException;
+use Kartenmacherei\RestFramework\Action\Query\QueryLocator;
+use Kartenmacherei\RestFramework\Framework;
+use Kartenmacherei\RestFramework\Request\Request;
+use Kartenmacherei\RestFramework\ResourceRequest\ResourceRequest;
+use Kartenmacherei\RestFramework\ResourceRequest\ResourceRequestHandler;
+use Kartenmacherei\RestFramework\Response\MethodNotAllowedResponse;
+use Kartenmacherei\RestFramework\Response\NotFoundResponse;
+use Kartenmacherei\RestFramework\Response\Response;
+use Kartenmacherei\RestFramework\RestResource\RestResource;
+use Kartenmacherei\RestFramework\Router\NoMoreRoutersException;
+use Kartenmacherei\RestFramework\Router\Router;
+use Kartenmacherei\RestFramework\Router\RouterChain;
+
+class FrameworkTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateInstanceReturnsExpectedObject()
+    {
+        $this->assertInstanceOf(Framework::class, Framework::createInstance());
+    }
+
+    public function testReturnsNotFoundResponseWhenNoMoreRoutersExceptionIsThrown()
+    {
+        $routerChain = $this->getRouterChainMock();
+        $routerChain->method('route')->willThrowException(new NoMoreRoutersException());
+        $framework = new Framework($routerChain, $this->getResourceRequestHandlerMock());
+
+        $this->assertInstanceOf(NotFoundResponse::class, $framework->run($this->getRequestMock()));
+    }
+
+    public function testReturnsMethodNotAllowedResponseWhenNoMoreLocatorsExceptionIsThrown()
+    {
+        $routerChain = $this->getRouterChainMock();
+        $routerChain->method('route')->willReturn($this->getResourceRequestMock());
+
+        $requestHandler = $this->getResourceRequestHandlerMock();
+        $requestHandler->method('handle')->willThrowException(new NoMoreLocatorsException());
+
+        $framework = new Framework($routerChain, $requestHandler);
+
+        $this->assertInstanceOf(MethodNotAllowedResponse::class, $framework->run($this->getRequestMock()));
+    }
+
+    public function testReturnsResponseFromResourceHandler()
+    {
+        $routerChain = $this->getRouterChainMock();
+        $routerChain->method('route')->willReturn($this->getResourceRequestMock());
+
+        $response = $this->getResponseMock();
+
+        $requestHandler = $this->getResourceRequestHandlerMock();
+        $requestHandler->method('handle')->willReturn($response);
+
+        $framework = new Framework($routerChain, $requestHandler);
+
+        $this->assertSame($response, $framework->run($this->getRequestMock()));
+    }
+
+    public function testRegisterResourceAddsRouter()
+    {
+        $router = $this->getRouterMock();
+        $restResource = $this->getRestResourceMock();
+        $restResource->method('getRouter')->willReturn($router);
+
+        $routerChain = $this->getRouterChainMock();
+        $routerChain->expects($this->once())->method('addRouter')->with($this->identicalTo($router));
+
+        $framework = new Framework($routerChain, $this->getResourceRequestHandlerMock());
+
+        $framework->registerResource($restResource);
+    }
+
+    public function testRegisterResourceAddsCommandLocator()
+    {
+        $commandLocator = $this->getCommandLocatorMock();
+
+        $restResource = $this->getRestResourceMock();
+        $restResource->method('hasCommandLocator')->willReturn(true);
+        $restResource->method('getCommandLocator')->willReturn($commandLocator);
+
+        $requestHandler = $this->getResourceRequestHandlerMock();
+        $requestHandler->expects($this->once())->method('registerCommandLocator')->with($this->identicalTo($commandLocator));
+
+        $framework = new Framework($this->getRouterChainMock(), $requestHandler);
+
+        $framework->registerResource($restResource);
+    }    
+    
+    public function testRegisterResourceAddsQueryLocator()
+    {
+        $queryLocator = $this->getQueryLocatorMock();
+
+        $restResource = $this->getRestResourceMock();
+        $restResource->method('hasQueryLocator')->willReturn(true);
+        $restResource->method('getQueryLocator')->willReturn($queryLocator);
+
+        $requestHandler = $this->getResourceRequestHandlerMock();
+        $requestHandler->expects($this->once())->method('registerQueryLocator')->with($this->identicalTo($queryLocator));
+
+        $framework = new Framework($this->getRouterChainMock(), $requestHandler);
+
+        $framework->registerResource($restResource);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|CommandLocator
+     */
+    private function getCommandLocatorMock()
+    {
+        return $this->createMock(CommandLocator::class);
+    }    
+    
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|QueryLocator
+     */
+    private function getQueryLocatorMock()
+    {
+        return $this->createMock(QueryLocator::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|RestResource
+     */
+    private function getRestResourceMock()
+    {
+        return $this->createMock(RestResource::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Router
+     */
+    private function getRouterMock()
+    {
+        return $this->createMock(Router::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Resource
+     */
+    private function getResponseMock()
+    {
+        return $this->createMock(Response::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|ResourceRequestHandler
+     */
+    private function getResourceRequestHandlerMock()
+    {
+        return $this->createMock(ResourceRequestHandler::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|ResourceRequest
+     */
+    private function getResourceRequestMock()
+    {
+        return $this->createMock(ResourceRequest::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|RouterChain
+     */
+    private function getRouterChainMock()
+    {
+        return $this->createMock(RouterChain::class);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject|Request
+     */
+    private function getRequestMock()
+    {
+        return $this->createMock(Request::class);
+    }
+}
