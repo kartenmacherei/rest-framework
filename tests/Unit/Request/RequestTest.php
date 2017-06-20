f3b357a5a5b54b694ef80581e278c7c2404cff08
Lee Wilkins <lee.wilkins@kartenmacherei.de>
2017-06-20 16:07:58 +0200

    master: added tests for Request

--- tests/Unit/Request/RequestTest.php
+++ tests/Unit/Request/RequestTest.php
@@ -3,7 +3,9 @@ namespace Kartenmacherei\RestFramework\UnitTests\Request;
 
 use Kartenmacherei\RestFramework\Request\DeleteRequest;
 use Kartenmacherei\RestFramework\Request\GetRequest;
+use Kartenmacherei\RestFramework\Request\Header\Header;
 use Kartenmacherei\RestFramework\Request\Header\HeaderCollection;
+use Kartenmacherei\RestFramework\Request\Header\HeaderException;
 use Kartenmacherei\RestFramework\Request\Method\RequestMethod;
 use Kartenmacherei\RestFramework\Request\Method\UnsupportedRequestMethodException;
 use Kartenmacherei\RestFramework\Request\OptionsRequest;
@@ -59,6 +61,40 @@ class RequestTest extends PHPUnit_Framework_TestCase
         $this->assertSame($uri, $request->getUri());
     }
 
+    public function testProperlyDeterminesIfHeaderExists()
+    {
+        $uri = new Uri('/foo');
+        $request = $this->getAbstractRequest($uri);
+
+        $this->assertTrue($request->hasHeader('TEST_HEADER'));
+    }
+
+    public function testProperlyDeterminesIfHeaderDoesNotExist()
+    {
+        $uri = new Uri('/foo');
+        $request = $this->getAbstractRequest($uri, false);
+
+        $this->assertFalse($request->hasHeader('BAD_TEST_HEADER'));
+    }
+
+    public function testCanGetHeader()
+    {
+        $uri = new Uri('/foo');
+        $request = $this->getAbstractRequest($uri);
+
+        $this->assertInstanceOf(Header::class, $request->getHeader('TEST_HEADER'));
+    }
+
+    public function testThrowsExceptionIfHeaderDoesNotExist()
+    {
+        $uri = new Uri('/foo');
+        $request = $this->getAbstractRequest($uri, false);
+
+        $this->expectException(HeaderException::class);
+
+        $request->getHeader('BAD_TEST_HEADER');
+    }
+
     /**
      * @return array
      */
@@ -76,19 +112,30 @@ class RequestTest extends PHPUnit_Framework_TestCase
 
     /**
      * @param Uri $uri
+     * @param bool $hasHeader
+     *
      * @return Request|\PHPUnit_Framework_MockObject_MockObject
      */
-    private function getAbstractRequest(Uri $uri)
+    private function getAbstractRequest(Uri $uri, bool $hasHeader = true)
     {
-        return $this->getMockForAbstractClass(Request::class, [$uri, $this->getHeaderCollectionMock()]);
+        return $this->getMockForAbstractClass(Request::class, [$uri, $this->getHeaderCollectionMock($hasHeader)]);
     }
 
     /**
-     * @return \PHPUnit_Framework_MockObject_MockObject|HeaderCollection
+     * @param bool $hasHeader
+     *
+     * @return HeaderCollection|\PHPUnit_Framework_MockObject_MockObject
      */
-    private function getHeaderCollectionMock()
+    private function getHeaderCollectionMock(bool $hasHeader = true)
     {
-        return $this->createMock(HeaderCollection::class);
+        $headerCollection = $this->createMock(HeaderCollection::class);
+        $headerCollection->method('has')->willReturn($hasHeader);
+        if ($hasHeader) {
+            $headerCollection->method('get')->willReturn($this->createMock(Header::class));
+        } else {
+            $headerCollection->method('get')->willThrowException(new HeaderException);
+        }
+        return $headerCollection;
     }
 
     /**
